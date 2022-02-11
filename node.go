215b84b822a71c277a97009644d42f1dfacee20d
Megha Mattikalli <megharm2@illinois.edu>
2022-02-11 14:11:31 -0600

    mapping

--- node.go
+++ node.go
@@ -8,6 +8,12 @@ import (
     "strconv"
 )
 
+type node struct {
+    node_name string
+    host_name string
+    port_num int
+  }
+
 func main() {
     content, err := os.ReadFile("config.txt")
     if err != nil {
@@ -20,7 +26,26 @@ func main() {
     content3 := strings.Split(content2, "\n")
 
     fmt.Println(content3[0])
-    nodes_num, i := strconv.Atoi(content3[0])
-    // fmt.Println(string(content))
+    nodes_num, err := strconv.Atoi(content3[0])
+    fmt.Println(string(nodes_num))
+
+    m := make(map[string]node)
+
+    for i := 1; i <= nodes_num; i++ {
+        node_info := strings.Split(content3[i], " ")
+        port, port_err := strconv.Atoi(node_info[2])
+        if port_err != nil {
+            log.Fatal(port_err)
+        }
+        new_node := node {
+            node_name: node_info[0],
+            host_name: node_info[1],
+            port_num: port,
+        }
+
+        m[node_info[0]] = new_node
+    }
+
+    fmt.Println(m)
 
 }
