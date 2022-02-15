0a4f8d343fc6aeae30794f5e9be06c32d5245538
Nisha Pant <nishap2@illinois.edu>
2022-02-14 18:35:27 -0600

    args

--- node.go
+++ node.go
@@ -1,51 +1,73 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "strings"
-    "strconv"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type node struct {
-    node_name string
-    host_name string
-    port_num int
-  }
+	node_name string
+	host_name string
+	port_num  string
+}
 
 func main() {
-    content, err := os.ReadFile("config.txt")
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println(string(content))
+	args := os.Args[1:]
+	node_name := args[0]
+	config_file := args[1]
 
-    content2 := string(content)
+	print(node_name)
 
-    content3 := strings.Split(content2, "\n")
+	content, err := os.ReadFile(config_file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(content))
 
-    fmt.Println(content3[0])
-    nodes_num, err := strconv.Atoi(content3[0])
-    fmt.Println(string(nodes_num))
+	content2 := string(content)
 
-    m := make(map[string]node)
+	content3 := strings.Split(content2, "\n")
 
-    for i := 1; i <= nodes_num; i++ {
-        node_info := strings.Split(content3[i], " ")
-        port, port_err := strconv.Atoi(node_info[2])
-        if port_err != nil {
-            log.Fatal(port_err)
-        }
-        new_node := node {
-            node_name: node_info[0],
-            host_name: node_info[1],
-            port_num: port,
-        }
+	fmt.Println(content3[0])
+	nodes_num, err := strconv.Atoi(content3[0])
+	// fmt.Println(string(nodes_num))
 
-        m[node_info[0]] = new_node
-    }
+	m := make(map[string]node)
 
-    fmt.Println(m)
+	for i := 1; i <= nodes_num; i++ {
+		node_info := strings.Split(content3[i], " ")
+		// port, port_err := strconv.Atoi(node_info[2])
+		// if port_err != nil {
+		// 	log.Fatal(port_err)
+		// }
+		new_node := node{
+			node_name: node_info[0],
+			host_name: node_info[1],
+			port_num:  node_info[2],
+		}
 
+		m[node_info[0]] = new_node
+	}
+
+	fmt.Println(m)
+
+	self_node := m[node_name]
+
+	// thread 1
+	serv_port := ":" + self_node.port_num
+
+	ln, err := net.Listen("tcp", serv_port)
+
+	// handle any incoming connections
+
+	// thread 2
+	for k, v := range m {
+		if k != node_name {
+			conn, err = net.Dial("tcp", strings.Join([]string{v.host_name, v.port_num}, ":"))
+		}
+	}
 }
