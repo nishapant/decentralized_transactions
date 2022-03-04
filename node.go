993c40fdfebf0c20e0adde0082cf15f6e9a81512
Nisha Pant <nishap2@illinois.edu>
2022-03-03 23:40:42 -0600

    hi

--- node.go
+++ node.go
@@ -267,9 +267,14 @@ func send_req(host string, port string, name string) {
 
 }
 
-func recieve_conn_reqs(port string) {
+func recieve_conn_reqs(port_ string) {
+	number := 1
 	for i := 0; i < total_conns/2; i++ {
+		port_int, _ := strconv.Atoi(port_)
+		port_int = port_int + number
+		port := strconv.Itoa(port_int)
 		go recieve_req(port)
+		number++
 	}
 	wg.Wait()
 }
